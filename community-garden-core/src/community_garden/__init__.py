3a9af8b04478a3c2d6383958875733fecd5049fc
Artem Letyushev <artemletyushev@gmail.com>
2026-06-28 23:15:23 +0300

    Remove duplicate community-garden-core directory

--- community-garden-core/src/community_garden/__init__.py
+++
@@ -1,4 +0,0 @@
-from community_garden.project import GardenProject
-from community_garden.models import CommunityEvent, Actor, Content, Space, Relation
-
-__all__ = ["GardenProject", "CommunityEvent", "Actor", "Content", "Space", "Relation"]
